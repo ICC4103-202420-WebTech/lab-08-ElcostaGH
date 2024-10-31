b650d71e8aded5620ff9099917dfa51dd8fd5838
Jose Costa <jicosta@miuandes.cl>
2024-10-31 13:48:48 -0300

    Added css and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "welcome", to: "static_pages#welcome"
-  get "about", to: "static_pages#about"
   get "training_sessions/index"
   get "training_sessions/new"
   get "training_sessions/create"
